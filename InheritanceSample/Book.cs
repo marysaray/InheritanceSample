a23f857bcc54922ac8f19eece5a62f326820a1b5
Mary Saray <msaray.ms@outlook.com>
2021-08-31 20:43:00 -0700

    Created book class inherits from product

---
+++ InheritanceSample/Book.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceSample
+{
+    public class Book : Product
+    {
+        /* Properties */
+
+        /// <summary>
+        /// 13 digit serial code associated with the book.
+        /// </summary>
+        public string Isbn { get; set; }
+
+        /// <summary>
+        /// Name of Author for a specific book.
+        /// </summary>
+        public string Author { get; set; }
+    }
+}
