0e65d6e01981a194ef99027fd6c2d6948d22cca0
Mary Saray <msaray.ms@outlook.com>
2021-09-01 05:28:56 -0700

    Added author to book constructor for requirements

--- InheritanceSample/Program.cs
+++ InheritanceSample/Program.cs
@@ -16,11 +16,10 @@ namespace InheritanceSample
                 Price = 9.99
             };
 
-            Book b = new Book("Murach's C#", "1563245689754")
+            Book b = new Book("Murach's C#", "1563245689754", "Joel Murach")
             {
                 ProductId = 2,
                 Price = 39.99,
-                Author = "Joel Murach",
             };
 
             Software s = new Software("Visual Studio 2019")
