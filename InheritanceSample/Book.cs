9cfaf9c145352eb0bd0ca797bedc0cd4c08b04b7
Mary Saray <msaray.ms@outlook.com>
2021-09-01 05:13:34 -0700

    Added overload method for isbn requirement and exception error message

--- InheritanceSample/Book.cs
+++ InheritanceSample/Book.cs
@@ -8,9 +8,16 @@ namespace InheritanceSample
 {
     public class Book : Product
     {
+        /* Constructors */
+
         public Book(string productName) : base(productName) // inherits the base class constructor
         {
-            // inherits from product class
+            throw new InvalidOperationException("ISBN is required to add book.");
+        }
+
+        public Book(string productName, string isbn) : base(productName) // overload methods with isbn required &  inherits the base class constructor
+        {
+            Isbn = isbn;
         }
 
         /* Properties */
