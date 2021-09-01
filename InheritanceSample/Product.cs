417e68f55b10b4c760ccb3ebcd743422120c76a8
Mary Saray <msaray.ms@outlook.com>
2021-08-31 20:08:17 -0700

    Created product class

---
+++ InheritanceSample/Product.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceSample
+{
+    /// <summary>
+    /// Represents a salable object.
+    /// </summary>
+    class Product
+    {
+        /* properties */
+
+        public int ProductId { get; set; }
+
+        public string Name { get; set; }
+
+        public double Price { get; set; }
+
+        /* methods */
+
+        /// <summary>
+        /// Return a display string that represents current product.
+        /// </summary>
+        /// <returns></returns>
+        public string GetDisplayText()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
