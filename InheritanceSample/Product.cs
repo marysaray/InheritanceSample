357a92825d1c5d712d24701a07dcacaa87923760
Mary Saray <msaray.ms@outlook.com>
2021-08-31 20:44:59 -0700

    Added documentation and GetDisplayText method

--- InheritanceSample/Product.cs
+++ InheritanceSample/Product.cs
@@ -9,25 +9,42 @@ namespace InheritanceSample
     /// <summary>
     /// Represents a salable object.
     /// </summary>
-    class Product
+    public class Product
     {
-        /* properties */
+        /* Properties */
 
+        /// <summary>
+        /// Primary Key for product.
+        /// </summary>
         public int ProductId { get; set; }
 
+        /// <summary>
+        /// Name of product.
+        /// </summary>
         public string Name { get; set; }
 
+        /// <summary>
+        /// Price of product.
+        /// </summary>
         public double Price { get; set; }
 
-        /* methods */
+        /* Methods */
 
         /// <summary>
         /// Return a display string that represents current product.
         /// </summary>
         /// <returns></returns>
+        // block syntax
         public string GetDisplayText()
         {
-            throw new NotImplementedException();
+            return $"{Name} costs {Price}";
         }
+
+        /* -- expression-bodied syntax:
+         * https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/statements-expressions-operators/expression-bodied-members
+         
+            public string GetDisplayText()
+                => $"{Name} costs {Price}";
+        */
     }
 }
