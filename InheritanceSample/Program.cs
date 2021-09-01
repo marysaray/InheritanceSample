edc760a6f1d63afc9df65d160173f5400db630cd
Mary Saray <msaray.ms@outlook.com>
2021-08-31 20:45:28 -0700

    Created instances for each class

--- InheritanceSample/Program.cs
+++ InheritanceSample/Program.cs
@@ -6,10 +6,34 @@ using System.Threading.Tasks;
 
 namespace InheritanceSample
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+            Product p = new Product()
+            {
+                ProductId = 1,
+                Name = "Gadget",
+                Price = 9.99
+            };
+
+            Book b = new Book()
+            {
+                ProductId = 2,
+                Name = "Murach's C#",
+                Price = 39.99,
+                Author = "Joel Murach",
+                Isbn = "1563245689754"
+            };
+
+            Software s = new Software()
+            {
+                ProductId = 3,
+                Name = "Visual Studio 2019",
+                Price = 500,
+                Productkey = "student@email.edu",
+                Version = "16.7.1"
+            };
         }
     }
 }
