aae93efb0928a529cd413ae4b8a9464cadca1e89
Mary Saray <msaray.ms@outlook.com>
2021-08-31 20:43:20 -0700

    Created software class inherits from product

---
+++ InheritanceSample/Software.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceSample
+{
+    public class Software : Product
+    {
+        /* Properties */
+
+        /// <summary>
+        /// Semantic version of software ex: 3.0.2
+        /// </summary>
+        public string Version { get; set; }
+
+        /// <summary>
+        /// Email associated to the downloadable software application.
+        /// </summary>
+        public string Productkey { get; set; }
+    }
+}
